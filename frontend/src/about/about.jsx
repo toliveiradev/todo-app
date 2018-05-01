21839ddf8513de562182d77d24887f9d0f06c170
Golden Gate Tecnologia <goldengatetecnologia@MacBook-Pro-de-Golden.local>
2018-05-01 02:47:04 -0300

    Final To-Do

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre'></PageHeader>
+
+        <h2>Nossa história</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Missão e visão</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+    </div>
+)
